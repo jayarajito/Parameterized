47440fb46cde8ff91a03b718b4fef7f3ef3a9deb
Jayaraj Narayanan <jayarajito@gmail.com>
2021-11-01 15:54:01 +0530

    MyThirdCommit

--- Simple.java
+++ Simple.java
@@ -1,9 +1,6 @@
-class Simple{  
-   // public static void main(String args[]){  
-     //System.out.println("Hello Java");
-   //  System.out.println("Your first argument is: "+args[0]); 	 
-   // }  
-   
+class Simple
+{
+	
      public static void main(String[] args)
     {
         // convert into integer type
